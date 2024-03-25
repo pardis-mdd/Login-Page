757d63b5a6e40b7164ce7ac988e9fe6bb87fa20b
Pardis <154790380+pardis-mdd@users.noreply.github.com>
2024-03-25 23:50:56 +0330

    Update app.py

--- app.py
+++ app.py
@@ -5,21 +5,16 @@ app = Flask(__name__)
 @app.route('/', methods=['GET', 'POST'])
 def index():
     if request.method == 'POST':
-        # Handle registration form submission
+        
         email = request.form['email']
         password = request.form['password']
-        # Process the registration data as needed
-        # For example, you can save it to a database
-        
-        # Redirect to the thank you page with the email as a parameter
+       
         return redirect(url_for('thank_you', email=email))
     
-    # Render the index.html template normally
     return render_template('index.html')
 
 @app.route('/thank_you/<email>')
 def thank_you(email):
-    # Render the thank you template with the email parameter
     return render_template('thank_you.html', email=email)
 
 if __name__ == "__main__":
